b94129d55025b06e10d3798d2563a2caabe34bf1
Carmen <vazquecm@gmail.com>
2015-10-29 13:13:54 -0500

    initial commit

---
+++ french.js
@@ -0,0 +1,10 @@
+var translator = function (){
+    var holidayMsg = {
+        "merry": "Joyeux",
+        "christmas": "noel",
+        "and": "et",
+        "a": "un",
+        "happy": "content",
+        "new": "neuf",
+        "year": "an"
+    }
