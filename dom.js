d22bb13a00a2ecb399325bea717d35afd6f0d50f
Carmen <vazquecm@gmail.com>
2015-10-29 16:59:51 -0500

    updates

---
+++ dom.js
@@ -0,0 +1,35 @@
+var inputbox = $("#input");
+var dropdown = $("#language");
+console.log(dropdown);
+
+
+
+
+
+
+// var transBtn = $("#trans-btn");
+
+var text; 
+var language;
+
+$("#trans-btn").click(function(e) {
+  text = inputbox.val();
+  language = dropdown.val(); 
+  console.log(text);
+  console.log(language);
+
+  if (language === "french") {
+    var thing = Translator.translateToFrench(text);
+    console.log(thing);
+  } else if (language === "german") {
+    var thing = Translator.translateToGerman(text);
+    console.log(thing);
+  } else if (language === "italian") {
+    var thing = Translator.translateToItalian(text);
+    console.log(thing);
+  }
+    // console.log("trans-btn", trans-btn);
+ })
+ 
+// });
+
--- french.js
+++ french.js
@@ -1,5 +1,6 @@
-var translator = function (){
-    var holidayMsg = {
+var Translator = (function(oldTranslator) {
+
+    var French = {
         "merry": "Joyeux",
         "christmas": "noel",
         "and": "et",
@@ -8,3 +9,41 @@ var translator = function (){
         "new": "neuf",
         "year": "an"
     }
+
+// variables used in loop //
+oldTranslator.translateToFrench = function(msg) {
+    var frenchMsg = [];
+    var frenchVersion = "";
+    var i;
+    var lowermsg = msg.toLowerCase();
+    var frenchArray = lowermsg.split(" ");
+    
+// beginning of loop //
+    for (i = 0; i < frenchArray.length; i++) {
+
+// takes the key and unlocks the value in french //
+    	for (var key in French) {
+
+// if the key equals the french word will input into the array //   		
+    		if (key === frenchArray[i]) {
+
+// pushing the french words into the empty french array to create msg //
+    			frenchMsg.push(French[key]);
+          console.log(frenchMsg);
+    		}
+      }
+     }
+
+// joins the french version into an array //
+      frenchVersion = frenchMsg.join(" ");
+
+  // returns translated string //
+      return frenchVersion;
+}
+
+  return Translator;
+
+})(Translator);
+
+
+
